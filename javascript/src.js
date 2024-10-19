02aa5d6556af62ade0cfa90d1d4a8c512cc59288
AmahleZuma <170299648+AmahleZuma@users.noreply.github.com>
2024-10-19 21:25:51 +0200

    Javascript section started

---
+++ javascript/src.js
@@ -0,0 +1,23 @@
+// Assignment
+
+// 1. Adding two numbers together
+console.log(10+2);
+
+// 2. Adding a sequence of 6 numbers together
+console.log(1+2+3+4+5+6);
+
+// 3. Sum
+console.log((4+6+9)/77);
+
+// 4. Using variables
+
+let a = 10;
+console.log(a);
+console.log(9*a);
+let b = 7*a;
+console.log(b);
+
+// 5. Using const
+const max = 57;
+const actual = max - 13;
+const percentage = actual/max;
