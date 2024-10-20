b3b2dbd25e0896e6f097bacc26212c33cb586200
AmahleZuma <170299648+AmahleZuma@users.noreply.github.com>
2024-10-20 21:26:47 +0200

    Javascript string methods p1

--- javascript/src.js
+++ javascript/src.js
@@ -21,3 +21,30 @@ console.log(b);
 const max = 57;
 const actual = max - 13;
 const percentage = actual/max;
+
+// String methods
+
+const text = "You are a massive idiot";
+
+// Checking length
+console.log(text.length);
+// Looking for a specific word
+console.log(text.charAt(2));
+// Looking for a UTF-8 Code
+console.log(text.charCodeAt(2));
+// Trying out ES2022 feature: Apparently this is more succinct and readable
+const text1 = text.at(2);
+console.log(text1)
+//Another way of looking
+const msg = text;
+console.log(msg[2]);
+console.log(text);
+
+// Looking at slicing. Basically "cuts" out the string of characters you want and puts them forward, ignoring the rest
+const fruits = "Apple, Banana, Kiwi";
+const sliced_fruit = fruits.slice(7, 13);
+console.log(sliced_fruit);
+const sliced_fruits = fruits.slice(7);
+console.log(sliced_fruits);
+
+// Will continue tomorrow
