b27bc61dec81ccffae1b1b2dfae613f10e61cd94
AmahleZuma <170299648+AmahleZuma@users.noreply.github.com>
2024-10-27 02:02:18 +0200

    Starting out with functions

---
+++ javascript/functions.js
@@ -0,0 +1,10 @@
+// Function that automates a greeting toward a user
+
+// Better way to write this?
+const userName = "Amahle"
+
+function greetings() {
+    alert(`Hello ${userName}, welcome back to portshare!`);
+}
+
+greetings();
