395d6c6ac2cd0a0186e5f9c3e787a6fe52df0b9d
AmahleZuma <170299648+AmahleZuma@users.noreply.github.com>
2024-10-30 07:23:44 +0200

    Starting Rock Paper Scissors, added function for playing the game and will build using functions

---
+++ javascript/rps.js
@@ -0,0 +1,13 @@
+const rock = 1;
+const paper = 2;
+const scissors = 3;
+
+function ask_question() {
+    const choice = prompt("Choose: rock(1), paper(2), scissors(3)");
+}
+
+
+
+function main() {
+
+}
