b08d68fd4ddfc6d93626e7aed6fa2ebf48819664
AmahleZuma <170299648+AmahleZuma@users.noreply.github.com>
2024-11-02 23:18:24 +0200

    Rock Paper Scissors Assignment complete

--- rock paper scissors/rps.js
+++ rock paper scissors/rps.js
@@ -1,19 +1,47 @@
-const rock = 1;
-const paper = 2;
-const scissors = 3;
-// I made the choice variable global
-const choice = prompt("Choose: rock(1), paper(2), scissors(3)");
+// Making it easy for both PC and user to pick
+const choices = ["rock", "paper", "scissors"]
+let userChoice;
+let computerChoice;
 
-
-// Creating a function that displays/tests for what I have picked
-function display(option) {
-    console.log(`Your choice is ${choice}`)
+// User turn
+function usr_turn() {
+    userChoice = prompt("Choose: rock, paper, scissors").toLowerCase();
+    return userChoice;
 }
 
 
+
+// Creating a function for PC to pick a choice
+function pc_choice() {
+    computerChoice = choices[(Math.floor(Math.random() * choices.length))];
+    return computerChoice
+}
+
+// Comparison function using if statements
+function compare(user, pc) {
+    if (computerChoice == userChoice) {
+        console.log("It is a tie")
+    } else if (computerChoice == "rock" && userChoice == "paper") {
+        console.log("Paper beats rock. You WIN");
+    } else if (computerChoice == "paper" && userChoice == "rock") {
+        console.log("Paper beats rock. You LOSE");
+    } else if (computerChoice == "rock" && userChoice == "scissors") {
+        console.log("Rock beats scissors. You LOSE");
+    } else if (computerChoice == "scissors" && userChoice == "rock") {
+        console.log("Rock beats scissors. You WIN");
+    } else if (computerChoice == "paper" && userChoice == "scissors") {
+        console.log("Scissors beats paper. You WIN");
+    } else if (computerChoice == "scissors" && userChoice == "paper") {
+        console.log("Scissors beats paper. You LOSE");
+    } 
+}
+
 // Main function to start game off
 function main() {
-    display(choice);
+    usr_turn();
+    pc_choice();
+    compare(userChoice, computerChoice);
+
 }
 
 main();
